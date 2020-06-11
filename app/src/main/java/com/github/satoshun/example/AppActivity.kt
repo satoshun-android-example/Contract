4ebb367accc24225b1ca3b1664cf0059bbb35f08
SatoShun <shun.sato1@gmail.com>
2020-06-11 10:41:51 +0000

    permissions

--- app/src/main/java/com/github/satoshun/example/AppActivity.kt
+++ app/src/main/java/com/github/satoshun/example/AppActivity.kt
@@ -1,6 +1,7 @@
 package com.github.satoshun.example
 
 import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
@@ -15,9 +16,17 @@ class AppActivity : AppCompatActivity(R.layout.app_act) {
     Toast.makeText(this@AppActivity, "isGranted $isGranted", Toast.LENGTH_LONG).show()
   }
 
+  private val requestPermissions = registerForActivityResult(
+    ActivityResultContracts.RequestMultiplePermissions(),
+    arrayOf(ACCESS_FINE_LOCATION, READ_EXTERNAL_STORAGE)
+  ) { grants ->
+    Toast.makeText(this@AppActivity, "grants $grants", Toast.LENGTH_LONG).show()
+  }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
-    requestLocation.launch(Unit)
+//    requestLocation.launch(Unit)
+    requestPermissions.launch(Unit)
   }
 }
